3d38b11d352ff9e551b36310182a4330fff9e550
sethvincent <sethvincent@gmail.com>
2016-05-11 20:52:34 -0700

    fix index.js to use http

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+var http = require('http')
 var config = require('./config')
 var staticland = require('./lib/server')(config)
 
-staticland.listen(config.port, function () {
+http.createServer(staticland).listen(config.port, function () {
   staticland.log('listening on 127.0.0.1:' + config.port)
 })
