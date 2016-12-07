910b41dc32ffae3bbba57c0752db8733c7f28494
sethvincent <sethvincent@gmail.com>
2016-12-07 11:35:53 -0800

    owner vs owners bug fix

--- index.js
+++ index.js
@@ -2,21 +2,6 @@ var http = require('http')
 var config = require('./config')
 var staticland = require('./lib/server')(config)
 
-config.hooks = {
-  deploy: function (opts, cb) {
-    console.log('hooks.deploy', opts)
-    cb(null, opts)
-  },
-  createUser: function (opts, cb) {
-    console.log('hooks.createUser', opts)
-    cb(null, opts)
-  },
-  addOwner: function (opts, cb) {
-    console.log('hooks.addOwner', opts)
-    cb(null, opts)
-  }
-}
-
 http.createServer(staticland).listen(config.port, function () {
   console.log('listening on 127.0.0.1:' + config.port)
 })
