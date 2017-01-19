d0ba64371f313d643394455eaa76e90e597bcc5e
sethvincent <sethvincent@gmail.com>
2017-01-19 11:19:48 -0800

    reset password, use township/township

---
+++ tests/site-handlers.js
@@ -0,0 +1,8 @@
+// var fs = require('fs')
+// var test = require('tape')
+// var config = require('../example.config')
+// var db = require('memdb')()
+//
+// test('get sites by accountKey', function (t) {
+//
+// })
